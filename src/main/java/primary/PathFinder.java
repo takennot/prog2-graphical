28d72cfdd71d76ad63da04f52df5056c5c55747b
SagaLD <sagaliljenrothdickman@gmail.com>
2022-05-13 13:49:16 +0200

    app

---
+++ src/main/java/primary/PathFinder.java
@@ -0,0 +1,16 @@
+package primary;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PathFinder extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
