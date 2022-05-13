d127bad2484d7c6f5da6a207a06a41422469a769
SagaLD <sagaliljenrothdickman@gmail.com>
2022-05-13 14:04:23 +0200

    blub

--- src/main/java/primary/PathFinder.java
+++ src/main/java/primary/PathFinder.java
@@ -5,30 +5,28 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class PathFinder extends Application {
 
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        //our main pane idk
+        Pane root = new Pane();
 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+        //title of stage
+        primaryStage.setTitle("PathFinder");
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        //dropDown
+
+        //button Find Path
+
+
         primaryStage.setScene(new Scene(root, 300, 250));
         primaryStage.show();
     }
