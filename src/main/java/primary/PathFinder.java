677ae49945641705a30009293287b5353d5c900c
takennot <rus.musaev1996@gmail.com>
2022-05-13 14:34:37 +0200

    buttons

--- src/main/java/primary/PathFinder.java
+++ src/main/java/primary/PathFinder.java
@@ -7,10 +7,12 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import org.controlsfx.control.action.Action;
 
 public class PathFinder extends Application {
 
@@ -30,25 +32,51 @@ public class PathFinder extends Application {
         // Array with actions
         String[] fileActions = {"New Map", "Open", "Save", "Save Image", "Exit"};
         // combobox declaration
-        ComboBox fileDropDown = new ComboBox(FXCollections.observableArrayList(fileActions));
-        Label testSelected = 
+        ComboBox fileCombobox = new ComboBox(FXCollections.observableArrayList(fileActions));
+        // test label to see if event handler is working
+        Label testSelected = new Label("default selected");
+        // creates action event
+        EventHandler<ActionEvent> fileEvent = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent){
+                testSelected.setText(fileCombobox.getValue() + " is selected");
+            }
+        };
+        fileCombobox.setOnAction(fileEvent);
+        // second event handler event for buttons
+        EventHandler<ActionEvent> buttonsEvent = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent){
+                testSelected.setText(fileCombobox.getValue() + " is selected");
+            }
+        };
         // sticks root pane to top border
-        root.setTop(fileDropDown);
+        root.setTop(fileCombobox);
 
         //FlowPane for buttons
-        Pane flow = new FlowPane();
+        Pane buttonsFlowPane = new FlowPane();
 
         //button Find Path
-        Button findPathButton = new Button("FindPath");
+        Button findPathButton = new Button("Find Path");
+        buttonsFlowPane.getChildren().add(findPathButton);
 
         //button Show Connection
+        Button showConnectionButton = new Button("Show Connection");
+        buttonsFlowPane.getChildren().add(showConnectionButton);
 
         //button New Place
+        Button newPlaceButton = new Button("New Place");
+        buttonsFlowPane.getChildren().add(newPlaceButton);
 
         //button New Connection
+        Button newConnection = new Button("New Connection");
+        buttonsFlowPane.getChildren().add(newConnection);
 
         //button Change Connection
+        Button changeConnection = new Button("Change Connection");
+        buttonsFlowPane.getChildren().add(changeConnection);
 
+        root.setCenter(buttonsFlowPane);
 
         primaryStage.setScene(new Scene(root, 300, 250));
         primaryStage.show();
